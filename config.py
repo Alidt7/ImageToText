8ca451742726729afc8408ac33ac153fca5c693e
Ali <alidianati2005@gmail.com>
2025-08-03 18:31:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from typing import final
 
-TOKEN: final = '7808563469:AAHFlpN6x4EKI5zBwkIHellWGvFKYEM6L84'
-BOT_USERNAME: final = '@alifirstpythontlbot'
-ADMIN_ID: final = 7996716892
-ADMIN_USERNAME: final = '@botadminali'
+TOKEN: final = ''
+BOT_USERNAME: final = '@'
+ADMIN_ID: final = 0
+ADMIN_USERNAME: final = '@'
 
